9b2009a84c178bc6fb64557336fe4cfa78a56fab
wuzyatbareuloh <127809230+wuzyatbareuloh@users.noreply.github.com>
2023-09-06 11:35:24 +0900

    Add files via upload

---
+++ lib/controllerForTimeStamp.dart
@@ -0,0 +1,28 @@
+// //from https://www.youtube.com/watch?v=x2CoecrzBCA
+
+import './visitorTimeStamp.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+class ControllerForTimeStamp {
+  final void Function(String) callback;
+
+  static const String URL =
+      "https://script.google.com/macros/s/AKfycbwDh23coA0b6JN2JIQY7lGfvhqOed3vDa6_zLlmWEQ-wUBDal_p5OmqjmdWbKHYTnsC/exec";
+
+  static const STATUS_SUCCESS = "SUCCESS";
+
+  ControllerForTimeStamp(this.callback);
+
+  void submitTimeStampForm(VisitorTimeStampForm visitorTimeStampForm) async {
+    try {
+      await http
+          .get(Uri.parse(URL + visitorTimeStampForm.toParams()))
+          .then((response) {
+        callback(convert.jsonDecode(response.body)['status']);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+}
