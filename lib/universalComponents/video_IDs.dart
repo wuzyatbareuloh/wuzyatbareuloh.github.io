9dd66445b3dd568e6b8e44fc4c22b474bb463b36
wuzyatbareuloh <127809230+wuzyatbareuloh@users.noreply.github.com>
2024-04-30 16:45:00 -0400

    Add files via upload

---
+++ lib/universalComponents/video_IDs.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class VideoIDs {
+  static const latest_release_video_id =
+      //"hhttps://www.youtube.com/watch?v=I5l3lypU9jQ";
+      "I5l3lypU9jQ";
+}
